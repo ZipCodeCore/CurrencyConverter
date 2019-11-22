3fc8fadc2cd9f3fe8bd34c92cb43e4c0659c4c56
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-22 12:28:45 -0500

    Add logic for Australian

--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/AustralianDollar.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/AustralianDollar.java
@@ -1,6 +1,12 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 
 public class AustralianDollar implements ConvertableCurrency {
+
+    public Double convert (CurrencyType currencyType){
+
+        return currencyType.getRate()/CurrencyType.AUSTRALIAN_DOLLAR.getRate();
+    }
 }
