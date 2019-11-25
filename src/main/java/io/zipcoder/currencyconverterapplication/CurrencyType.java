8469bb3183c4c42a901553aa423b85107da65172
vaidehi4321 <56602405+vaidehi4321@users.noreply.github.com>
2019-11-25 14:22:53 -0500

    currency converter done

--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
@@ -25,6 +25,7 @@ public enum CurrencyType {
     }
 
     public static CurrencyType getTypeOfCurrency(ConvertableCurrency currency) {
-        return null;
+
+        return currency.getType();
     }
 }
