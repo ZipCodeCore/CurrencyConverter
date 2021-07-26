abc97747411fc52b30901164a305caeef192ae78
Nathan <nathan@zipcodes-MacBook-Pro-9.local>
2021-07-26 14:08:43 -0400

    passed tests

--- src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
@@ -1,7 +1,11 @@
 package io.zipcoder.currencyconverterapplication;
 
 public interface ConvertableCurrency {
+
+
     default Double convert(CurrencyType currencyType) {
-        return Double.MAX_VALUE;
+        //make this hoe call the currencyConverter?
+        return currencyType.getRate()/CurrencyType.getTypeOfCurrency(this).getRate();
+        //return Double.MAX_VALUE;
     }
 }
