e9a5773851b1a1ac1979506c09d11f3a475900d6
sitarameez <sitarablossom@gmail.com>
2021-07-25 21:20:55 -0400

    completed with the help of leon

--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/Ringgit.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/Ringgit.java
@@ -1,6 +1,11 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 
 public class Ringgit implements ConvertableCurrency {
+    public  Double convert(CurrencyType currencyType) {
+        Double result= currencyType.getRate()/CurrencyType.RINGGIT.getRate();
+        return result;
+    }
 }
