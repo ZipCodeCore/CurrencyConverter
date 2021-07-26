89d5c30868ec61497d2eaf5c594bef9337a03378
Dee <dee@zipcodes-MBP-2.fios-router.home>
2021-07-25 21:12:48 -0400

    enum get type corrected

--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyConverter.java
@@ -4,4 +4,5 @@ public class CurrencyConverter {
     public static Double convert(Double amountOfBaseCurrency, ConvertableCurrency sourceCurrencyType, CurrencyType targetCurrencyType) {
         return sourceCurrencyType.convert(targetCurrencyType) * amountOfBaseCurrency;
     }
+
 }
--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/AustralianDollar.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/AustralianDollar.java
@@ -1,6 +1,15 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 
 public class AustralianDollar implements ConvertableCurrency {
+
+    @Override
+    public Double convert(CurrencyType currencyType) {
+
+        return currencyType.getRate() /CurrencyType.AUSTRALIAN_DOLLAR.getRate();
+    }
+
+
 }
